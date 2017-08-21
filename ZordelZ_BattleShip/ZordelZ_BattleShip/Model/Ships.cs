e047e7c803d97f5f05f5a0685dfb9aea732554a2
zzordel <zzordel@student.neumont.edu>
2017-08-21 16:24:06 -0600

    Created Main

---
+++ ZordelZ_BattleShip/ZordelZ_BattleShip/Model/Ships.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZordelZ_BattleShip.Model
+{
+    class Ships
+    {
+    }
+}
